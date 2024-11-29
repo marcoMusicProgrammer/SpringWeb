f02b61815a3cad9aed7033e5131dfcbd2a5ca5dc
marco <marco.maisto@hotmail.it>
2024-11-29 10:25:14 +0100

    first commit

---
+++ src/test/java/com/generation/freelancerspring/FreelancerSpringApplicationTests.java
@@ -0,0 +1,15 @@
+package com.generation.freelancerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerSpringApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
